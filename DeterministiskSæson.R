4aa9b8bdd07ee14b01365e10c383e107b8d84f6e
Your Name <you@example.com>
2020-05-04 16:00:34 +0200

    Rettelser

--- DeterministiskSæson.R
+++ DeterministiskSæson.R
@@ -7,7 +7,7 @@ library(FitAR)
 library(tseries)
 
 
-### Sæsonkorrigering af pris
+### S?sonkorrigering af pris
 y_t = DK1$P
 t = 1:length(y_t)
 
@@ -28,17 +28,17 @@ s8 = sin(24 * pi * omega_year * t)
 spris <- lm(y_t ~ t + I(t^2) + s1 + s2 + s3 + s4 + wkd)
 summary(spris)
 
-### Sæsonkorrigeret
+### S?sonkorrigeret
 skp <- y_t-fitted(spris)
-plot(dato ,skp, main = 'Fjernet sæson', type = "l", xlab = "Dato", ylab = "Pris i DKK")
+plot(dato ,skp, main = 'Fjernet s?son', type = "l", xlab = "Dato", ylab = "Pris i DKK")
 
 par(mfrow = c(1,1))
-### Deterministisk sæson
-plot(dato,y_t, type = "l", main = "Sæson ved pris", xlab = "Dato", ylab = "Pris i DKK")
+### Deterministisk s?son
+plot(dato,y_t, type = "l", main = "S?son ved pris", xlab = "Dato", ylab = "Pris i DKK")
 lines(dato, fitted(spris), lwd = 2, col = "dark blue")
 
 
-### Sæsonkorrigering af forbrug
+### S?sonkorrigering af forbrug
 y_t = DK1$C
 t = 1:length(y_t)
 
@@ -47,13 +47,13 @@ t = 1:length(y_t)
 sforbrug <- lm(y_t ~ t + I(t^2)+s1 + s2 + s3 + s4 + wkd)
 summary(sforbrug)
 
-### Sæsonkorrigeret
+### S?sonkorrigeret
 skf <- y_t-fitted(sforbrug)
-plot(dato ,skf, main = 'Fjernet sæson', type = "l", xlab = "Dato", ylab = "Forbrug i MWh")
+plot(dato ,skf, main = 'Fjernet s?son', type = "l", xlab = "Dato", ylab = "Forbrug i MWh")
 
 par(mfrow = c(1,1))
-### Deterministisk sæson
-plot(dato,y_t, type = "l", main = "Sæson ved forbrug", xlab = "Dato", ylab = "forbrug i MWh")
+### Deterministisk s?son
+plot(dato,y_t, type = "l", main = "S?son ved forbrug", xlab = "Dato", ylab = "forbrug i MWh")
 lines(dato, fitted(sforbrug), lwd = 2, col = "dark blue")
 
 ### ADF test
@@ -61,11 +61,11 @@ adf.test(skp) #Afviser nulhypotese
 adf.test(skf) #Afviser nulhypotese
 
 ### ACF
-acf(skp) #Ser ikke stationært ud
-acf(skf) #Der er stadig noget ugentlig sæson 
+acf(skp) #Ser ikke station?rt ud
+acf(skf) #Der er stadig noget ugentlig s?son 
 
 
-### SARMA på sæsonkorrigeret data
+### SARMA p? s?sonkorrigeret data
 sarima_model_AIC = Inf
 for (p in c(0,1,2,3,4)) for (q in c(0,1,2,3,4)) for (P in c(0,1,2,3,4)) for (Q in c(0,1,2,3,4)) {
   sarima_model = tryCatch(sarima(xdata = skp, p, d=0, q, P, D=0, Q, S = 7, details = FALSE, Model = F), error = function(e) Inf)
@@ -80,9 +80,49 @@ for (p in c(0,1,2,3,4)) for (q in c(0,1,2,3,4)) for (P in c(0,1,2,3,4)) for (Q i
     best_model = sarima_model
   }
 }
-### Bedste model ifølge AIC.
+### Bedste model if?lge AIC.
 best_model$fit
 
-### Bedste modeller ifølge AIC
+### Bedste modeller if?lge AIC
 sarmaskp <- sarima(xdata = skp, 3, 0, 3, 2, 0, 2, S = 7, details = FALSE, Model = F)
 sarmaskf <- sarima(xdata = skf, 3, 0, 3, 3, 0, 4, S = 7, details = FALSE, Model = F)
+
+newskp <- sarmaskp$fit$residuals
+newskf <- sarmaskf$fit$residuals
+
+### VAR med SARMA-data
+library("dse")
+library("vars")
+
+### Matrix med data
+vardat <- matrix(c(newskp, newskf), nrow = 2556, ncol = 2)
+colnames(vardat) <- c("pris", "forbrug")
+plot.ts(vardat)
+
+### Informationskriterier
+infocrit <- VARselect(vardat, lag.max = 25, type = "none")
+infocrit 
+
+### Estimer model
+varest <- VAR(vardat, p = 5, type = "none", season = NULL, exogen = NULL)
+summary(varest)
+
+### Diagnostiske tests
+
+### Portmanteau-test - test om der er korrelation i residualer
+
+varport <- serial.test(varest, lags.pt = 16, type = "PT.asymptotic")
+varport
+
+plot(varport, names = "pris")
+plot(varport, names = "forbrug")
+
+
+### Test - heteroskedacity
+vararch <- arch.test(varest, lags.multi = 5, multivariate.only = TRUE)
+vararch # Afviser nulhypotese
+
+
+### Test - normalitet
+varnorm <- normality.test(varest, multivariate.only = TRUE)
+varnorm$jb.mul # Afviser nulhypotese
