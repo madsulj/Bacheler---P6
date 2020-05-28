b6b47d9cd86dd54cd6325a9d8730a226086c361b
Your Name <you@example.com>
2020-05-29 01:03:50 +0200

    Ret

--- VarModel1.R
+++ VarModel1.R
@@ -1,8 +1,9 @@
+### VAR med SARMA-data
 library("dse")
 library("vars")
 
 ### Matrix med data
-vardat <- matrix(c(skp, skf), nrow = 2556, ncol = 2)
+vardat <- matrix(c(armadata, armadata2), ncol = 2)
 colnames(vardat) <- c("pris", "forbrug")
 plot.ts(vardat)
 
@@ -11,30 +12,66 @@ infocrit <- VARselect(vardat, lag.max = 25, type = "none")
 infocrit 
 
 ### Estimer model
-varest <- VAR(vardat, p = 7, type = "none", season = NULL, exogen = NULL)
+varest <- VAR(vardat, p = 8, type = "none", season = NULL, exogen = NULL)
 summary(varest)
+resvar2 <- restrict(varest, method = "ser")
+modsum <- summary(resvar2)
+modsum$covres
 
-### Tjekker r?dder
-roots <- roots(varest)
-roots # Alle mindre end 1
 
+### Residual plot. pris
+plot(resvar2)
+acf(resvar2$varresult$pris$residuals)
+ts.plot(resvar2$varresult$pris$residuals)
+qqnorm(resvar2$varresult$pris$residuals)
+qqline(resvar2$varresult$pris$residuals)
+
+### forbrug
+acf(resvar2$varresult$forbrug$residuals)
+ts.plot(resvar2$varresult$forbrug$residuals)
+qqnorm(resvar2$varresult$forbrug$residuals)
+qqline(resvar2$varresult$forbrug$residuals)
 
 ### Diagnostiske tests
 
-### Portmanteau-test - test om der er korrelation i residualer
+### Breusch-Godfrey test - test om der er korrelation i residualer
 
-varport <- serial.test(varest, lags.pt = 16, type = "PT.asymptotic")
-varport # Afviser nulhypotese
+bgtest <- serial.test(resvar2, type = "BG")
+bgtest
 
-plot(varport, names = "pris")
-plot(varport, names = "forbrug")
+plot(bgtest, names = "pris")
+plot(bgtest, names = "forbrug")
 
 
 ### Test - heteroskedacity
-vararch <- arch.test(varest, lags.multi = 5, multivariate.only = TRUE)
-vararch # Afviser nulhypotese
+vararch <- arch.test(resvar2, lags.multi = 5, multivariate.only = TRUE)
+vararch # Afviser nulhypotese. Der er heteroskedacity
 
 
 ### Test - normalitet
-varnorm <- normality.test(varest, multivariate.only = TRUE)
+varnorm <- normality.test(resvar2, multivariate.only = TRUE)
 varnorm$jb.mul # Afviser nulhypotese
+
+plot(resvar2)
+qqPlot(resvar2$varresult$forbrug$residuals)
+
+### Cholesky og reduceret form til strukturel
+### Ligning1
+A <- matrix(data = c( 3737,10543, 10543, 5076977), nrow = 2)
+ch <- t(chol(A))
+dd <- diag(ch)
+dm <- matrix(data= c(61.13101, 0,0,2246.60468), nrow = 2)
+d <- dm^2
+dm1 <- matrix(data= c(1/61.13101, 0,0,1/2246.60468), nrow = 2)
+l <- ch%*%dm1
+linv <- matrix(data = c( 1, -2.82, 0, 1), nrow = 2)
+
+### Ligning2
+A <- matrix(data = c( 5076977,10543, 10543, 3737), nrow = 2)
+ch <- t(chol(A))
+dd <- diag(ch)
+dm <- matrix(data= c(2253.31481, 0,0,60.95167), nrow = 2)
+d <- dm^2
+dm1 <- matrix(data= c(1/2253.31481, 0,0,1/60.95167), nrow = 2)
+l <- ch%*%dm1
+linv <- matrix(data = c( 1, -0.002076537, 0, 1), nrow = 2)
